7fc1b62a03045d738bccb2036877edd9f156c27c
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2023-04-30 23:54:40 -0300

    Adicionando o Lombok no projeto e refatorando os models

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -1,44 +1,24 @@
 package com.algaworks.ecommerce.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
 
+    @EqualsAndHashCode.Include
     @Id
     private Integer id;
 
     private String nome;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Cliente cliente = (Cliente) o;
-
-        return id.equals(cliente.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
 }
--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -1,13 +1,22 @@
 package com.algaworks.ecommerce.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Produto {
 
+    @EqualsAndHashCode.Include
     @Id
     private Integer id;
 
@@ -17,48 +26,4 @@ public class Produto {
 
     private BigDecimal preco;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Produto produto = (Produto) o;
-        return Objects.equals(id, produto.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
